c980889758fb86014740ecc0abb4e6596c0cb238
ggoswami777 <gaurav.goswami1304@gmail.com>
2026-03-19 22:54:02 +0530

    added basic routing

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,30 @@
 import { Hono } from 'hono'
 
-const app = new Hono()
+const app = new Hono().basePath('/api/v1')
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
+})
+app.post("/user/signup",(c)=>{
+  return c.text("signup");
+})
+app.post("/user/login",(c)=>{
+  return c.text("login");
+})
+app.post("/blog",(c)=>{
+  return c.text("blog post");
+})
+app.put("/blog",(c)=>{
+  return c.text("blog put");
+})
+
+
+app.get("/blog/:id",(c)=>{
+  const id=c.req.param('id');
+  return c.text("Get Blog:"+id);
+})
+app.get("/blog/bulk",(c)=>{
+  
 })
 
 export default app
