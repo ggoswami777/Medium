1a005d5cdff193fcb3c40561c53e9ee82d34fc8c
ggoswami777 <gaurav.goswami1304@gmail.com>
2026-03-20 12:30:38 +0530

    added the login and signup hono routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,67 @@
 import { Hono } from 'hono'
-
-const app = new Hono().basePath('/api/v1')
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+import { decode, sign, verify } from 'hono/jwt'
+const app = new Hono<{
+	Bindings: {
+		DATABASE_URL: string
+    JWT_SECRET:string
+	}
+}>().basePath('/api/v1')
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
-app.post("/user/signup",(c)=>{
-  return c.text("signup");
+app.post("/user/signup", async (c) => {
+  const prisma = new PrismaClient({
+    accelerateUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate())
+
+  try {
+    const body = await c.req.json();
+    console.log("Signup Body:", body);
+    
+    if (!body.email || !body.password) {
+      return c.json({ error: "Email and password are required" }, 400);
+    }
+
+    const user = await prisma.user.create({
+      data: {
+        email: body.email,
+        password: body.password
+      }
+    });
+
+    const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
+    return c.json({ jwt });
+  } catch (e: any) {
+    console.error("Signup Error Detailed:", e);
+    
+    return c.json({ error: "Internal server error", message: e.message }, 500);
+  }
 })
-app.post("/user/login",(c)=>{
-  return c.text("login");
+app.post("/user/login",async(c)=>{
+   const prisma = new PrismaClient({
+    accelerateUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate())
+    const body=await c.req.json();
+    try {
+      const user=await prisma.user.findUnique({
+        where:{
+          email:body.email,
+          password:body.password
+        }
+      })
+      if(!user){
+        c.status(403);
+		    return c.json({ error: "user not found" });
+      }
+      const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
+      return c.json({ jwt });
+    } catch (error) {
+      return c.status(403);
+    }
+ 
 })
 app.post("/blog",(c)=>{
   return c.text("blog post");
@@ -22,9 +74,6 @@ app.put("/blog",(c)=>{
 app.get("/blog/:id",(c)=>{
   const id=c.req.param('id');
   return c.text("Get Blog:"+id);
-})
-app.get("/blog/bulk",(c)=>{
-  
 })
 
 export default app
