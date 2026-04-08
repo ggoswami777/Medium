371fc479ec7b9ffd0e08fd23bd6a8bd531dbebdb
ggoswami777 <gaurav.goswami1304@gmail.com>
2026-04-08 19:41:08 +0530

    fix:improved fetching blog from backend

--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -112,6 +112,15 @@ blogRouter.get("/:id", async (c) => {
       where: {
         id: id,
       },
+      select:{
+        title:true,
+        content:true,
+        author:{
+          select:{
+            name:true
+          }
+        }
+      }
     });
     return c.json({
       id: blog,
